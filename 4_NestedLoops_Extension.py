aafffb0adb19a091d1edddd24e042e6650a2973e
Shane Thurston <36428573+shane-thurston@users.noreply.github.com>
2021-03-18 14:35:31 +1100

    Create 4_NestedLoops_Extension.py

---
+++ 4_NestedLoops_Extension.py
@@ -0,0 +1,24 @@
+import turtle
+
+Pat = turtle.Turtle()
+
+Pat.color("blue")
+Pat.pensize(4)
+
+Pat.forward(20)
+Pat.left(90)
+Pat.forward(20)
+Pat.left(90)
+Pat.forward(10)
+Pat.left(90)
+Pat.forward(10)
+Pat.right(90)
+Pat.forward(10)
+Pat.right(90)
+Pat.forward(20)
+Pat.right(90)
+Pat.forward(30)
+Pat.right(90)
+Pat.forward(30)
+Pat.left(90)
+Pat.forward(10)
