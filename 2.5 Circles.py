7a6b3ccc5446ad8f612b1cd161266ed4c0ba9f7b
Shane Thurston <36428573+shane-thurston@users.noreply.github.com>
2022-03-02 12:49:48 +1100

    Create 2.5 Circles.py

---
+++ 2.5 Circles.py
@@ -0,0 +1,13 @@
+import turtle
+
+Bob = turtle.Turtle()
+Bob.shape("circle")
+
+Bob.color("blue")
+Bob.stamp()
+Bob.left(45)
+Bob.forward(50)
+Bob.stamp()
+Bob.right(90)
+Bob.forward(50)
+Bob.stamp()
